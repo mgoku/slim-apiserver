2ac6c87e7f7fb1295c0ee3f6ecfe44c5c51151e7
Misbachul Munir <misbachul.munir@gmail.com>
2016-12-30 10:48:29 +0700

    Buat middleware tuk cek type user

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,22 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+/****************************************************************/
+/*  Middleware untuk cek apakan user yang login adalah type ADMIN
+/****************************************************************/
+$checkAdmin = function ($request, $response, $next) {
+
+    $token = $request->getAttribute("token");
+
+    if ($token->type === 'ADMIN') {
+        return $next($request, $response);
+    } else {
+        return $response
+            ->withHeader('Content-type','application/json')
+            ->withStatus(401)
+            ->write(json_encode(array(
+              "msg" => "Unauthorized access"
+            ), JSON_NUMERIC_CHECK));
+    }
+};
